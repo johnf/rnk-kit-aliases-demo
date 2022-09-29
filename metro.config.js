c0ab41bac707a024a1b714f7e00ccb74ddc393cf
John Ferlito <johnf@inodes.org>
2022-09-30 07:37:46 +1000

    Enable rnx-start

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,13 @@
  *
  * @format
  */
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 
-module.exports = {
+module.exports = makeMetroConfig({
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -14,4 +19,4 @@ module.exports = {
       },
     }),
   },
-};
+});
